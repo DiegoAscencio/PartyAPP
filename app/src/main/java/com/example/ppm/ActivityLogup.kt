1af3e6019b6a939dcf8e97e5cbe0214205a43174
DiegoAscencio <is698355@iteso.mx>
2019-11-28 20:02:42 -0600

    A better UX with login

--- app/src/main/java/com/example/ppm/ActivityLogup.kt
+++ app/src/main/java/com/example/ppm/ActivityLogup.kt
@@ -9,11 +9,14 @@ import android.os.Build
 import android.util.Log
 import android.view.View
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import com.google.android.material.textfield.TextInputEditText
 import com.parse.ParseObject
+import java.time.MonthDay
+import java.util.*
 
-class ActivityLogup: AppCompatActivity() {
+class ActivityLogup : AppCompatActivity() {
     private lateinit var bBack: TextView
     private lateinit var etDate: TextInputEditText
     private lateinit var bDate: ImageButton
@@ -21,7 +24,10 @@ class ActivityLogup: AppCompatActivity() {
     private lateinit var tUsername: TextInputEditText
     private lateinit var tEmail: TextInputEditText
     private lateinit var tPassword: TextInputEditText
-    private lateinit var date: DatePickerDialog
+    private lateinit var tPassword1: TextInputEditText
+
+    private lateinit var fullDate: String
+
 
     @RequiresApi(Build.VERSION_CODES.N)
     //private var c = Calendar.getInstance()
@@ -32,12 +38,13 @@ class ActivityLogup: AppCompatActivity() {
         setContentView(R.layout.activity_logup)
 
         //Mostrar fecha picker
-        etDate =find(R.id.activity_logup_tf_birthday)
+        etDate = find(R.id.activity_logup_tf_birthday)
         bDate = find(R.id.activity_logup_btn_getDate)
 
         tUsername = find(R.id.activity_logup_tf_username_tx)
         tEmail = find(R.id.activity_logup_tf_email_tx)
         tPassword = find(R.id.activity_logup_tf_password_tx)
+        tPassword1 = find(R.id.activity_logup_tf_confpassword_tx)
 
 
         //Hide action bar
@@ -52,47 +59,68 @@ class ActivityLogup: AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun clicklup(view: View) {
-        //aqui va el parse
+        //validaciones
 
-        val gameScore = ParseObject("UserPPM")
-        gameScore.put("username", tUsername.text.toString())
-        gameScore.put("password", tPassword.text.toString())
-        //gameScore.put("email", tEmail.text.toString())
-        //gameScore.put("birthday","")
+        if (tUsername.text!!.isNotBlank() &&
+            tPassword.text!!.isNotBlank() &&
+            tEmail.text!!.isNotBlank() &&
+            etDate.text!!.isNotBlank() &&
+            tPassword1.text!!.isNotBlank() &&
+            tPassword.text.toString().equals(tPassword1.text.toString())
+        ) {
+                val gameScore = ParseObject("UserPPM")
+                gameScore.put("username", tUsername.text.toString())
+                gameScore.put("password", tPassword.text.toString())
+                gameScore.put("email", tEmail.text.toString())
+                gameScore.put("birthday", fullDate)
 
-        gameScore.saveInBackground{e -> if (e == null) {
-            // Hooray! Let them use the app now.
-            Log.d("Si jala", "Si hala")
+                gameScore.saveInBackground { e ->
+                    if (e == null) {
+                        // Hooray! Let them use the app now.
+                        Log.d("Si jala", "Si hala")
+                        Toast.makeText(
+                            this,
+                            "Usuario registrado",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        onBackPressed()
+                    } else {
+                        Log.d("No jala", "No hala")
+                        // Sign up didn't succeed. Look at the ParseException
+                        // to figure out what went wrong
+                    }
+                }
         } else {
-            Log.d("No jala", "No hala")
-            // Sign up didn't succeed. Look at the ParseException
-            // to figure out what went wrong
-        }}
+            Toast.makeText(
+                this,
+                "Las contraseñas deben de coincidir y ningun campo debe estar vacío",
+                Toast.LENGTH_LONG
+            ).show()
+        }
 
 
     }
 
-    /*
-    @RequiresApi(Build.VERSION_CODES.N)
+
     fun clickDataPicker(view: View) {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
 
-        val mes = c.get(Calendar.MONTH)
-        val dia = c.get(Calendar.DAY_OF_MONTH)
-        val anio = c.get(Calendar.YEAR)
-
-         date = DatePickerDialog(
+        val dpd = DatePickerDialog(
             this,
-            DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-                val mesActual = month + 1
-                val diaFormateado =
-                    if (dayOfMonth < 10) "0" + dayOfMonth.toString() else dayOfMonth.toString()
-                val mesFormateado =
-                    if (mesActual < 10) "0" + mesActual.toString() else mesActual.toString()
-                etDate.setText(diaFormateado + "/" + mesFormateado + "/" + year)
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                // Display Selected date in Toast
+                //Toast.makeText(this, """$dayOfMonth - ${monthOfYear + 1} - $year""", Toast.LENGTH_LONG).show()
+                fullDate = """$dayOfMonth / ${monthOfYear + 1} / $year"""
+                etDate.setText(fullDate)
             },
-            anio, mes, dia
+            year,
+            month,
+            day
         )
-        date.show()
+        dpd.show()
     }
-    */
+
 }
